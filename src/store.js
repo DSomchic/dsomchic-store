67cc5fed1c3383b9bf161b1a537f34a542129def
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2018-08-25 15:56:48 +0700

    implement web3

--- src/store.js
+++ src/store.js
@@ -1,16 +1,58 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import getWeb3 from '@/utils/getWeb3.js'
 
 Vue.use(Vuex)
-
-export default new Vuex.Store({
-  state: {
-
+const state = {
+  web3: {
+    isInjected: false,
+    web3Instance: null,
+    networkId: null,
+    coinbase: null,
+    balance: null,
+    error: null
   },
-  mutations: {
+  contractInstance: null
+}
 
-  },
-  actions: {
+const getters = {
 
+}
+
+const mutations = {
+  registerWeb3Instance (state, payload) {
+    console.log('registerWeb3instance Mutation being executed', payload)
+    let result = payload
+    console.log('result:', result)
+    let web3Copy = state.web3
+    web3Copy.coinbase = result.coinbase
+    web3Copy.networkId = result.networkId
+    web3Copy.balance = parseInt(result.balance, 10)
+    web3Copy.isInjected = result.injectedWeb3
+    web3Copy.web3Instance = result.web3
+    state.web3 = web3Copy
   }
-})
+}
+
+const actions = {
+  registerWeb3 ({ commit }) {
+    console.log('registerWeb3 Action being executed')
+    getWeb3.then(result => {
+      console.log('committing result to registerWeb3Instance mutation')
+      commit('registerWeb3Instance', result)
+    }).catch(e => {
+      console.log('error in action registerWeb3', e)
+    })
+  }
+}
+
+const store = () => {
+  return new Vuex.Store({
+    state,
+    getters,
+    actions,
+    mutations
+  })
+}
+
+export default store
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <p>Metamask: {{ web3.isInjected }}</p>
+    <p>Network: {{ web3.networkId }}</p>
+    <p>Account: {{ web3.coinbase }}</p>
+    <p>Balance: {{ web3.balance }}</p>
   </div>
 </template>
 
@@ -13,6 +17,15 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  computed: {
+    web3 () {
+      return this.$store.state.web3
+    }
+  },
+  beforeCreate () {
+    console.log('registerWeb3 Action dispatched from casino-dapp.vue')
+    this.$store.dispatch('registerWeb3')
   }
 }
 </script>
